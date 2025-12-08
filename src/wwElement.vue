3bed47c34824962066f80d3fa9be521674c3314d
Mehdi Tebourbi <mehdi.tebourbi@gmail.com>
2025-12-08 11:23:20 +0100

    v0.0.35: Update version

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -111,7 +111,7 @@
       <header v-if="content.showTopbar" class="ww-topbar" :style="topbarStyle">
         <div class="ww-topbar-left">
           <button v-if="content.allowCollapse" class="ww-topbar-btn" @click="toggleCollapse">
-            <svg class="ww-svg-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="4" y1="6" x2="20" y2="6"/><line x1="4" y1="12" x2="20" y2="12"/><line x1="4" y1="18" x2="20" y2="18"/></svg>
+            ☰
           </button>
 
           <div v-if="content.showSearch" class="ww-search-container" :style="searchContainerStyle">
